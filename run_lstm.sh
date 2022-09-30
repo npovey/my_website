e935786bfd3b0febed9e4fc94196af9078e35789
Ubuntu <ubuntu@ip-172-31-31-30.us-west-2.compute.internal>
2022-09-30 07:27:53 +0000

    all working

--- run_lstm.sh
+++ run_lstm.sh
@@ -6,7 +6,7 @@ cd /home/ubuntu/my_website/sherpa
   --port 6006 \
   --max-batch-size 50   --max-wait-ms 5 \
   --nn-pool-size 1 \
-  --nn-encoder-filename ./icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/exp/encoder_jit_trace-iter-472000-avg-18.pt \
-  --nn-decoder-filename ./icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/exp/decoder_jit_trace-iter-472000-avg-18.pt \
-  --nn-joiner-filename ./icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/exp/joiner_jit_trace-iter-472000-avg-18.pt  \
-  --bpe-model-filename ./icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/data/lang_bpe_500/bpe.model
+  --nn-encoder-filename ~/k2_models/icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/exp/encoder_jit_trace-iter-472000-avg-18.pt \
+  --nn-decoder-filename ~/k2_models/icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/exp/decoder_jit_trace-iter-472000-avg-18.pt \
+  --nn-joiner-filename ~/k2_models/icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/exp/joiner_jit_trace-iter-472000-avg-18.pt  \
+  --bpe-model-filename ~/k2_models/icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/data/lang_bpe_500/bpe.model
