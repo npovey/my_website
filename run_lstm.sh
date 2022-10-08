9cc2c344416da655b9c41ebd5aa5f6a4f5368df0
npovey <you@example.com>
2022-10-07 23:38:23 -0700

    path fix to giga model

--- run_lstm.sh
+++ run_lstm.sh
@@ -6,7 +6,7 @@ cd /workspace/my_website/sherpa
   --port 6006 \
   --max-batch-size 50   --max-wait-ms 5 \
   --nn-pool-size 1 \
-  --nn-encoder-filename /workspace/k2_models/icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/exp/encoder_jit_trace-iter-472000-avg-18.pt \
-  --nn-decoder-filename /workspace/k2_models/icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/exp/decoder_jit_trace-iter-472000-avg-18.pt \
-  --nn-joiner-filename /workspace/k2_models/icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/exp/joiner_jit_trace-iter-472000-avg-18.pt  \
+  --nn-encoder-filename /workspace/k2_models/icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/exp/giga/encoder_jit_trace-iter-468000-avg-16-giga.pt \
+  --nn-decoder-filename /workspace/k2_models/icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/exp/giga/decoder_jit_trace-iter-468000-avg-16-giga.pt \
+  --nn-joiner-filename /workspace/k2_models/icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/exp/giga/joiner_jit_trace-iter-468000-avg-16-giga.pt  \
   --bpe-model-filename /workspace/k2_models/icefall-asr-librispeech-lstm-transducer-stateless2-2022-09-03/data/lang_bpe_500/bpe.model
