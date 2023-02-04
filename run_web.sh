501f1ed6605cf4779c958a41878417c842d73628
npovey <24726380+npovey@users.noreply.github.com>
2023-02-03 23:47:04 -0800

    Update run_web.sh

--- run_web.sh
+++ run_web.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # cd /workspace/my_website/sherpa/sherpa/bin/web
-cd sherpa/sherpa/bin/web
+# cd sherpa/sherpa/bin/web
+cd sherpa/bin/web
 
 python3 -m http.server 6008
 #python3 -m https.server 443
